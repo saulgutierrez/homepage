f065962d450ea6dd9b8f07e41272361856df81f9
Saúl Gutiérrez <sauladai@hotmail.com>
2024-01-09 12:59:15 -0600

    Added scroll efects in main pages wirh ScrollReveal.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.description', { delay: 100 });
+ScrollReveal().reveal('.hobbies-container', { delay: 700 });
+ScrollReveal().reveal('.contact-container', { delay: 800 });
+ScrollReveal().reveal('.container', { delay: 300 });
+
+ScrollReveal().reveal('.container-project', { delay: 100 });
+
+ScrollReveal().reveal('.line-4', { delay: 50 });
