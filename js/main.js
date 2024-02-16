00d31d0ef48b4f40bfe970c6750604baa92a8f8f
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2024-02-15 23:27:48 -0600

    Added some animations effects

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-ScrollReveal().reveal('.description', { delay: 100 });
-ScrollReveal().reveal('.hobbies-container', { delay: 700 });
-ScrollReveal().reveal('.contact-container', { delay: 800 });
-ScrollReveal().reveal('.container', { delay: 300 });
+if (screen.width < 768) {
+    ScrollReveal().reveal('.description', { delay: 100 });
+    ScrollReveal().reveal('.hobbies-container', { delay: 700 });
+    ScrollReveal().reveal('.contact-container', { delay: 800 });
+    ScrollReveal().reveal('.container', { delay: 300 });
 
-ScrollReveal().reveal('.container-project', { delay: 100 });
+    ScrollReveal().reveal('.container-project', { delay: 100 });
 
-ScrollReveal().reveal('.line-4', { delay: 50 });
+    ScrollReveal().reveal('.line-4', { delay: 50 });
+}
